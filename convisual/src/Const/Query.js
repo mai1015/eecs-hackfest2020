7f2e57c39c111df1530530fe919bec0e2f89f0bd
gademalon@gmail.com <gademalon@gmail.com>
2020-02-20 12:46:03 -0500

    query

--- convisual/src/Const/Query.js
+++ convisual/src/Const/Query.js
@@ -1,6 +1,6 @@
 const BaseQuery = "localhost:8000/"
 
-const Query = {
+const QuerySeries = {
     China:BaseQuery+"api/series/CN",
     Usa:BaseQuery+"api/series/US",
     Canada:BaseQuery+"api/series/CA",
@@ -10,6 +10,20 @@ const Query = {
     SouthAmerica:BaseQuery+"api/series?c=SA",
     Africa:BaseQuery+"api/series?c=AF",
     Oceania:BaseQuery+"api/series?c=OC",
+
+}
+
+
+const QueryLatest = {
+    China:BaseQuery+"api/latest/CN",
+    Usa:BaseQuery+"api/latest/US",
+    Canada:BaseQuery+"api/latest/CA",
+    Asia:BaseQuery+"api/latest?c=AS",
+    Europe:BaseQuery+"api/latest?c=EU",
+    NorthAmerica:BaseQuery+"api/latest?c=NA",
+    SouthAmerica:BaseQuery+"api/latest?c=SA",
+    Africa:BaseQuery+"api/latest?c=AF",
+    Oceania:BaseQuery+"api/latest?c=OC",
 
 }
 
