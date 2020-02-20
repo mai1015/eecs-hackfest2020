4664eefc542cbe018580bd3ba769a021c232e7c0
mai1015 <mai1015@users.noreply.github.com>
2020-02-19 22:02:25 -0500

    update api

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { getDB } from "../data/db";
+import {CovState} from "../data/model/CovState";
 const router = Router();
 
 router.get('/country',(req, res) => {
@@ -20,9 +21,37 @@ router.get('/latest', (req, res) => {
     const db = getDB();
     db.all('SELECT cd.* ' +
         'FROM covid_data cd INNER JOIN (SELECT id, MAX(updated_at) t FROM covid_data GROUP BY country, province) late ' +
-        'ON cd.id = late.id AND cd.updated_at = late.t ORDER BY cd.updated_at DESC').then(r => {
+        'ON cd.id = late.id AND cd.updated_at = late.t ORDER BY cd.updated_at DESC')
+        .then(r => res.json(r))
+
+});
+
+router.get('/series/:country/', (req, res) => {
+    const db = getDB();
+    db.all('')
+})
+
+router.get('/series/:country/:prov', (req, res) => {
+
+})
+
+router.post('/series', async (req, res) => {
+    if (req.body.list && req.body.list.length > 0) {
+        let sql = 'SELECT * FROM country WHERE '
+        for (let c of req.body.list) {
+            sql += `code='${c}' or `
+        }
+        const db = getDB()
+        // const ret = await db.all()
+    } else {
+        res.json({})
+    }
+})
+router.get('/news', (req, res) => {
+    const db = getDB();
+    db.all('SELECT * FROM news').then(r => {
         res.json(r);
-    });
+    })
 });
 
 export default router;
