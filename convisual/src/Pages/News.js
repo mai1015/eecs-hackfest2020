e124a276a657c968024d12b4c5f364457f8042f9
Denniswzm <denniswzm@gmail.com>
2020-02-20 09:57:48 -0500

    news

--- convisual/src/Pages/News.js
+++ convisual/src/Pages/News.js
@@ -1,28 +1,71 @@
-import React from 'react';
-import { Row, Col , Carousel} from 'antd';
-import './News.scss';
+import React from "react";
+import { Row, Col, Carousel, Card } from "antd";
+import axios from "axios";
+import "./News.scss";
 
 class News extends React.Component {
-    render(){
-        return(
-            <div>
-                <Carousel className = 'carousel1' autoplay style={{ width: 600,height:100}}>
-                <div>
-                <h3>1</h3>
-                </div>
-                <div>
-                <h3>2</h3>
-                </div>
-                <div>
-                <h3>3</h3>
-                </div>
-                <div>
-                <h3>4</h3>
-                </div>
-                </Carousel>
-            </div>
-        )
-    }
+  state = {
+    news: null
+  };
+
+  componentWillMount() {
+    axios.get("http://localhost:8000/api/news").then(r => {
+      this.setState({
+        news: r.data
+      });
+    });
+  }
+
+  render() {
+    let imgHolder = ""
+    return this.state.news ? (
+      <div>
+      <Row type="flex" justify="space-between" align="middle">
+      <Col span={4}>
+      {this.state.news.map((ele, idx) => (
+          <Card
+          style={{ width: "20em", height: "18em",marginRight:"2em", marginTop:"1em" }}
+          
+          hoverable
+          cover={
+            <image
+              variant="top"
+              src={
+                ele.cover === undefined
+                  ? ""
+                  : ele.cover
+              }
+              onError={e => {
+                e.target.onError = null;
+                e.target.src = imgHolder;
+              }}
+            />
+          }
+        >
+          {console.log(ele)}
+          <Card.Meta
+            title={ele.title}
+            extra={<a href='ele.url'>More</a>}
+            description={
+              <div>
+                <h4 style={{ color: "#e77641" }}>
+                  {"$" + ele.price}
+                </h4>
+                <hr />
+                <p>{ele.content}</p>
+              </div>
+            }
+          />
+        </Card>
+        
+      ))}
+      </Col>
+      </Row>
+      </div>
+    ) : (
+      <div>Loading</div>
+    );
+  }
 }
 
 export default News;
