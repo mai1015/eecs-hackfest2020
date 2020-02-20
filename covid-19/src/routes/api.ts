b568275d76398f31f7357522839358e8fb72ff13
mai1015 <mai1015@users.noreply.github.com>
2020-02-20 13:19:31 -0500

    Add api and add status data

--- covid-19/src/routes/api.ts
+++ covid-19/src/routes/api.ts
@@ -31,18 +31,40 @@ router.get('/latest', (req, res) => {
 
 router.get('/latest/:country', (req, res) => {
     const db = getDB();
-    db.all(`SELECT pp.name name, pp.lat lat, pp.lng lng, confirmed, updated_at FROM ((covid_data cd
+    db.all(`SELECT pp.name name, pp.lat lat, pp.lng lng, confirmed, recovered, updated_at FROM ((covid_data cd
         INNER JOIN (SELECT id, MAX(updated_at) t FROM covid_data GROUP BY country, province) late ON cd.id = late.id) rs
         INNER JOIN (SELECT id cid, name FROM country WHERE code='${req.params.country}') cc ON cc.cid = rs.country)
         INNER JOIN province pp ON pp.id == province`).then(r => {
-        let ret: Plot[] = []
-        for (let i of r) {
-            ret.push({
-                name: i.name,
-                value: [i.lng, i.lat, i.confirmed]
-            })
+        if (req.query.t && req.query.t === 'bar') {
+            let ret = {
+                names:[],
+                series: [{
+                    name: 'Confirmed',
+                    type: 'bar',
+                    data: []
+                }, {
+                    name: 'Cured',
+                    type: 'bar',
+                    data: []
+                }]
+            }
+
+            for (let i of r) {
+                ret.names.push(i.name)
+                ret.series[0].data.push(i.confirmed)
+                ret.series[1].data.push(i.recovered)
+            }
+            res.json(ret)
+        } else {
+            let ret: Plot[] = []
+            for (let i of r) {
+                ret.push({
+                    name: i.name,
+                    value: [i.lng, i.lat, i.confirmed]
+                })
+            }
+            res.json(ret)
         }
-        res.json(ret)
     })
 })
 
@@ -91,6 +113,14 @@ router.get('/series', (req, res) => {
         const db = getDB()
         db.all(`SELECT sum(confirmed) confirmed, sum(death) death, sum(recovered) recovered, updated_at FROM (covid_data cd
                 INNER JOIN (SELECT id cid FROM country WHERE continent='${req.query.c}') cc ON cc.cid = cd.country)
+                group by updated_at`).then(r => {
+            let series = generateTimeSeries(req.query.c, r)
+            res.json(series)
+        })
+    } else if (req.query.ct)  {
+        const db = getDB()
+        db.all(`SELECT sum(confirmed) confirmed, sum(death) death, sum(recovered) recovered, updated_at FROM (covid_data cd
+                INNER JOIN (SELECT id cid FROM country WHERE code='${req.query.ct}') cc ON cc.cid = cd.country)
                 group by updated_at`).then(r => {
             let series = generateTimeSeries(req.query.c, r)
             res.json(series)
