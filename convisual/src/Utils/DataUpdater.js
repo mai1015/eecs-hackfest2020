c9754f665ce6e5a711f9f04c3488bc51ce9a1f2f
gademalon@gmail.com <gademalon@gmail.com>
2020-02-20 12:00:40 -0500

    modified:   src/Utils/DataUpdater.js

--- convisual/src/Utils/DataUpdater.js
+++ convisual/src/Utils/DataUpdater.js
@@ -1,35 +1,58 @@
-import consts from '../Const/Const'
-import Fetcher from "./Fetcher"
+import consts from "../Const/Const";
+import Fetcher from "./Fetcher";
+const query = consts.queries;
+const updater = (graphType, contentName, optionFile) => {
+  switch (grpahType) {
+    case "geo3d":
+      return geo3dUpdater(contentName, optionFile);
+      break;
+    case "scatter":
+      return scatterUpdater(contentName, optionFile);
+      break;
+    case "axis":
+      return axisUpdater(contentName, optionFile);
+      break;
+    default:
+      return {};
+  }
+};
 
-const updater = (graphType,contentName,optionFile)=>{
-    switch(grpahType){
-        case "geo3d":
-            return geo3dUpdater(contentName,optionFile)
-            break;
-        case "scatter":
-            return scatterUpdater(contentName,optionFile) 
-            break;
-        case "axis":
-            return axisUpdater(contentName,optionFile)
-            break;
-        default:
-            return {}
-    }
-}
+geo3dUpdater = (contentName, optionFile) => {
+  if (contentName === "UsaCanada") {
+    return new Promise((resolve, reject) => {
+      Promise.all([Fetcher(query.Usa), Fetcher(query.Canada)])
+        .then(r => {
+          let newOption = { ...optionFile };
+          let newItem = newOption.series[0];
+          let newData = [...r[0], ...r[1]];
 
-geo3dUpdater = (contentName,optionFile)=>{
+          newItem.data = newData;
 
-}
+          newOption.series[0] = newItem.data;
+          resolve(newOption);
+        })
+        .catch(reject);
+    });
+  } else {
+    return new Promise((resolve, reject) => {
+      Fetcher(query[contentName])
+        .then(r => {
+          let newOption = { ...optionFile };
+          let newItem = newOption.series[0];
+          let newData = [...r];
 
-scatterUpdater = (contentName,optionFile)=>{
+          newItem.data = newData;
 
-}
+          newOption.series[0] = newItem.data;
+          resolve(newOption);
+        })
+        .catch(reject);
+    });
+  }
+};
 
-axisUpdater = (contentName,optionFile)=>{
+scatterUpdater = (contentName, optionFile) => {};
 
-}
-
-
-
+axisUpdater = (contentName, optionFile) => {};
 
 export default Updater;
