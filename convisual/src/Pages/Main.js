53d72d72954db738e60b59a69974b1e614be57b3
Denniswzm <denniswzm@gmail.com>
2020-02-20 12:48:48 -0500

    fix main page

--- convisual/src/Pages/Main.js
+++ convisual/src/Pages/Main.js
@@ -10,6 +10,11 @@ class Main extends React.Component {
             <QueueAnim delay={1000} className = "queue-simple">
             <div className= "showarea" key="main1">
                 <div className = 'maintext1'><p>Photo by Health Issues India</p></div>
+                    <div className = 'maintext2' key="main14">
+                        <p>The Situation is Rapidly Evolving and We are Closely Monitoring the Outbreak,</p>
+                        <p>Conducting Surveillance and Appropriate Laboratory Testing, </p>
+                        <p>and Providing Public Health and Infection Control Guidance.</p>
+                    </div>
             </div>
             </QueueAnim>
         )
